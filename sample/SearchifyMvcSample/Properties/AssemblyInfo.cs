ee32c38231713acd581f5031f9b9dd61cb275b42
Phil Oyston <Philo@users.noreply.github.com>
2019-10-04 10:26:22 +0100

    +semver:breaking - Users/philo/upgrade v6 (#3)
    * Upgrade to ES v6
    
    * Updates to release version of searchify library and updates nuget symbols format

--- sample/SearchifyMvcSample/Properties/AssemblyInfo.cs
+++ sample/SearchifyMvcSample/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SearchifyMvcSample")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SearchifyMvcSample")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -21,15 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d96603f7-bee4-4c6b-9f9c-8d37ccb0599c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/Nest.Searchify.Mvc/Properties/AssemblyInfo.cs
+++ src/Nest.Searchify.Mvc/Properties/AssemblyInfo.cs
@@ -1,16 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Nest.Searchify.Mvc")]
-[assembly: AssemblyDescription("Provides Mvc specific helpers for the Nest.Searchify library")]
-[assembly: AssemblyCompany("Phil Oyston, Storm ID Ltd")]
-
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-    [assembly: AssemblyConfiguration("Release")]
-#endif
-
-[assembly: AssemblyProduct("Nest.Searchify.Mvc")]
-
 [assembly: ComVisible(false)]
